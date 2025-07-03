5bbbdeb9d67bec5f5e1edb08b93a9e5dd3fd83b9
JuanAlfonsoDeveloper <juanalfonsoworkmail.com>
2025-07-03 05:35:19 -0500

    Se empezo a intentar subir a la tabla de usuarios datos

---
+++ backend/insertar_usuarios.py
@@ -0,0 +1,10 @@
+from conexion import conectar 
+
+nombreUsuario = "Juan"
+apellidoUsuario = "Alfonso"
+correoUsuario = "jhernandez@gmail.com"
+telefonoUsuario = "3112531749"
+contraseña = "123456789"
+direccionUsuario = "av siempre viva"
+idRol = 1
+
