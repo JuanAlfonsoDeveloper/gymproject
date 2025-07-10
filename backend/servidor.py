8ad915f059fcd99692a78eefb04dab138f885971
JuanAlfonsoDeveloper <juanalfonsoworkmail.com>
2025-07-10 05:34:52 -0500

    Se continuan haciendo cosas en el servidor poniendo menu

--- backend/servidor.py
+++ backend/servidor.py
@@ -1,8 +1,18 @@
+#menu
 def main():
     print("---Menu del servidor---")
     print("1. Registrar Usuario")
     print("2. Salir")
 
     opcion = input("Seleccione una opción: ")
+
+
+    #Opcines del menu
+    if opcion == "1":
+        nombre = input("Digite su nombre")
+        
+
+
+
 
 main()
