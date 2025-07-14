edb09d78dddd129c4d58bf452454198b3702f816
JuanAlfonsoDeveloper <juanalfonsoworkmail.com>
2025-07-14 05:35:16 -0500

    Se conecto la base de datos con el servidor, queda pendiente la inserción de datos

--- backend/servidor.py
+++ backend/servidor.py
@@ -1,4 +1,5 @@
 #menu
+from conexion import conectar
 def main():
     print("---Menu del servidor---")
     print("1. Registrar Usuario")
@@ -9,7 +10,18 @@ def main():
 
     #Opcines del menu
     if opcion == "1":
-        nombre = input("Digite su nombre")
+        nombre = input("Digite su nombre: ")
+        apellido = input("Digite su apellido: ")
+        correo = input("Digite su correo: ")
+        telefono = input("Digite su telefono ")
+        direccion = input("Digite su direccion: ")
+        contraseña = input("Digite su contraseña: ")
+        rol = input("Digite el codigo rol: ")
+        
+        conexion = conectar()
+        cursor = conexion.cursor()
+
+
         
 
 
